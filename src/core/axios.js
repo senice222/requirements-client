39e3ef8eec2c856e6050b86c9d34fb468142d018
senice222 <sasdasd920@gmail.com>
2024-09-09 18:16:56 +0300

    change url

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = 'http://localhost:4000/api';
+export const url = 'https://orders.consultantnlgpanel.ru/api';
 const $api = axios.create({
     baseURL: url,
     headers: {
