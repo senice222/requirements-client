ea3d653df24219a19e2953770bbed8080ca6a9f8
senice222 <sasdasd920@gmail.com>
2024-09-19 11:22:50 +0300

    diff text

--- src/components/ResponsiveHeader/ResponsiveHeader.jsx
+++ src/components/ResponsiveHeader/ResponsiveHeader.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import s from './ResponsiveHeader.module.scss'
 import {Menu} from './Svg'
 
 const ResponsiveHeader = ({setActive}) => {
   return (
     <div className={s.responsiveHeader}>
-        <h2>Панель актов</h2>
+        <h2>Панель требований</h2>
         <div onClick={() => setActive(true)}><Menu /></div>
     </div>
   )
